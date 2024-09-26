293cfe4af32ecdb0b3029451691fee786e1d6fdf
Silje Melling <silje.melling94@gmail.com>
2024-09-26 19:51:50 +0200

    testing deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,8 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+            Hei internett!!!
           </li>
-          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className={styles.ctas}>
