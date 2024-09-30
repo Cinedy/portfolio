626e0ed97eed610199c0e7aab4c509a8cd42a0e5
Silje Melling <silje.melling94@gmail.com>
2024-09-30 22:10:14 +0200

    changed menu to english

--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -8,10 +8,10 @@ export default function Menu() {
     
     return (
       <nav className={styles.container}>
-        <Link href="/" className={`${styles.menuButton} ${pathname == "/" ? styles.active : ""}`}>Hjem</Link>
+        <Link href="/" className={`${styles.menuButton} ${pathname == "/" ? styles.active : ""}`}>Home</Link>
         <Link href="/cv" className={`${styles.menuButton} ${pathname == "/cv" ? styles.active : ""}`}>CV</Link>
-        <Link href="/portfolio" className={`${styles.menuButton} ${pathname == "/portfolio" ? styles.active : ""}`}>Portefølje</Link>
-        <Link href="/contact" className={`${styles.menuButton} ${pathname == "/contact" ? styles.active : ""}`}>Kontakt meg</Link>
+        <Link href="/portfolio" className={`${styles.menuButton} ${pathname == "/portfolio" ? styles.active : ""}`}>Portfolio</Link>
+        <Link href="/contact" className={`${styles.menuButton} ${pathname == "/contact" ? styles.active : ""}`}>Contact Info</Link>
       </nav>
     );
   }
